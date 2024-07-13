b9b17632d182e505594cf2c090658a2435ab4db6
sumeet hibare <theforeverfella@gmail.com>
2024-07-13 20:39:00 +0530

    fixes

--- components/HomePage/Impress.jsx
+++ components/HomePage/Impress.jsx
@@ -5,20 +5,36 @@ function Impress() {
     return (
         <>
 
-            <div class="px-4 py-8 mx-auto flex items-center justify-center bg-zinc-100 w-full h-96">
-                <div class="mx-auto">
-                    <p className='p-2'>Shetty institute of technology's</p>
-                    <h1 class="mb-4 text-4xl tracking-tight leading-none">Department of Computer Science & Engineering</h1>
-                    <div className='p-4 flex gap-6'>
-                        <label className="input input-bordered flex items-center gap-2 btnanim">
-                            <input type="text" className="grow" placeholder="Enroll Now!" />
-                            <ChevronRightIcon className="h-4 w-4 text-zinc-950" />
-                        </label>
-                        <a className='p-3 btnanim bg-zinc-950 text-zinc-50 border-none'>linkedin</a>
+
+            <section className="bg-gray-50">
+                <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div className="mx-auto max-w-lg">
+                        <p className="hidden text-gray-500 sm:mt-4 sm:block p-4">
+                            Shetty institute of technology's
+                        </p>
+                        <h2 className="text-lg font-semibold text-gray-950 lg:text-3xl tracking-tight">
+                            Department of Computer Science & Engineering
+                        </h2>
+                    </div>
+
+                    <div className="mx-auto mt-8 max-w-xl">
+                        <form action="#" className="flex gap-4">
+                            <div className="sm:flex-1">
+                                <label className="sr-only">Email</label>
+
+                                <input
+                                    type="email"
+                                    placeholder="Email address"
+                                    className="w-full text-zinc-950 input input-bordered flex items-center gap-2 btnanim"
+                                />
+
+                            </div>
+
+                            <a className='p-3 btnanim bg-zinc-950 text-zinc-50 border-none'>linkedin</a>
+                        </form>
                     </div>
                 </div>
-
-            </div>
+            </section>
 
         </>
     )
--- modals/navigation/Navbar.jsx
+++ modals/navigation/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
+import { ComputerDesktopIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
 import menuData from './menuData';
 
 function Navbar() {
@@ -13,17 +13,17 @@ function Navbar() {
 
     return (
         <>
-            <label htmlFor="Slider" className="btn btn-circle btn-ghost drawer-button float-right absolute right-10 top-10 btnanim">
-                <EllipsisHorizontalIcon className="size-7 inline-block stroke-current" />
+            <label htmlFor="Slider" className="btn btn-circle btn-ghost drawer-button float-right absolute lg:right-10 lg:top-10 right-4 top-4 btnanim">
+                <ComputerDesktopIcon className="size-6" />
             </label>
 
             <div className="drawer">
                 <input id="Slider" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-side">
                     <label htmlFor="Slider" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu bg-zinc-950 min-h-full w-80 p-4" onClick={closeSidebar}>
+                    <ul className="menu bg-zinc-950 min-h-full lg:w-80 w-full p-4" onClick={closeSidebar}>
                         {menuData.map((item, index) => (
-                            <li className='rounded-lg btnanim text-zinc-50' key={index}><a href={item.href} onClick={closeSidebar}>{item.label}</a></li>
+                            <li className='text-2xl tracking-tight text-zinc-200' key={index}><a href={item.href} onClick={closeSidebar}>{item.label}</a></li>
                         ))}
                     </ul>
                 </div>
