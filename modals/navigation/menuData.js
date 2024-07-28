584b99d387daa441a2dc69464dd5b20fec420ef9
sumeet hibare <theforeverfella@gmail.com>
2024-07-28 16:23:53 +0530

    new

--- modals/navigation/menuData.js
+++ modals/navigation/menuData.js
@@ -7,7 +7,7 @@ const menuData = [
     { label: 'Gallery', href: '/gallery' },
     // { label: 'Events', href: '/events' },
     // { label: 'Student Center', href: '/' },
-    { label: 'Enrollment', href: '/enrollment' },
+    { label: 'Enrollment', href: 'https://forms.gle/JwLton1CngUeEBrX7' },
     // { label: 'Contact', href: '/' },
     { label: 'Clubs', href: '/clubs' },
 ];
